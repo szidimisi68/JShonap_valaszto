17342bc23a13e6695053d79e6d5bc823e44127f3
szidimisi68 <70961855+szidimisi68@users.noreply.github.com>
2024-09-05 10:45:50 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+var honapok = {
+    "Jan": 31,
+    "Feb": 28,
+    "Mar": 31,
+    "Apr": 30,
+    "Maj": 31,
+    "Jun": 30,
+    "Jul": 31,
+    "Aug": 31,
+    "Sze": 30,
+    "Okt": 31,
+    "Nov": 30,
+    "Dec": 31
+  };
+
+function Valasztott(){}
